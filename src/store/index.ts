0ba4e89e8cd121633742d4a389eec903c544f32c
kkulesa <katarzyna.kulesa@openanalytics.eu>
2022-10-28 15:02:26 +0200

    add: first stores

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import repositories, { RepositoryState } from "./repositories"
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+export interface State{
+  repositories: RepositoryState
+}
+
+export default new Vuex.Store<State>({
   modules: {
+    repositories: repositories
   }
 })
