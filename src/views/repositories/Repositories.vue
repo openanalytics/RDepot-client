3e9684888515af67f4d4d4f51ca50ea9ad7b284f
kkulesa <katarzyna.kulesa@openanalytics.eu>
2022-10-31 12:14:17 +0100

    add: temporary view to see if router works properly

---
+++ src/views/repositories/Repositories.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    {{title}}
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      title: 'repositories' 
+    }
+}
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
