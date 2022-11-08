f9ca35266fee137f756e3c302c9cb806ee0eee19
kkulesa <katarzyna.kulesa@openanalytics.eu>
2022-11-08 18:21:14 +0100

    change: adjust axios to respect keycloak token

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,17 +1,23 @@
 import axios, { AxiosRequestConfig } from "axios";
+import { updateToken } from "./keycloak";
 
 const api = axios.create({
     baseURL: 'http://localhost:8017/api',
     timeout: 6000,
   });
 
-  api.interceptors.request.use(function (config: AxiosRequestConfig) {
-    const token = localStorage.getItem("userToken") 
-    const multipart = localStorage.getItem("multipart") 
+  api.interceptors.request.use(function (config: AxiosRequestConfig) {let token = localStorage.getItem("userToken") 
+    const multipart = localStorage.getItem("multipart")
 
     if (token) {
       config.headers!.Authorization = `Token ${token}`
+    } else{
+      // updateToken();
+      token = localStorage.getItem('vue-token');
+      config.headers!.Authorization = `Token ${token}`;
     }
+
+    config.headers!.AccessControlAllowOrigin = '*'
 
     if(multipart == "true"){
       config.headers!['Content-Type'] = "multipart/form-data";
