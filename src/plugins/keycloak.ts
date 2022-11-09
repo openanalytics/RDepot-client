10962f202caf2a287324d059a3acabdb556ba388
kkulesa <katarzyna.kulesa@openanalytics.eu>
2022-11-09 16:59:10 +0100

    change: keycloak plugin with initial method

--- src/plugins/keycloak.ts
+++ src/plugins/keycloak.ts
@@ -1,5 +1,13 @@
+import store from '@/store';
 import Keycloak from 'keycloak-js'
 import Vue from 'vue';
+import router from '@/router'
+import vuetify from '@/plugins/vuetify'
+import Notifications from 'vue-notification'
+import VueI18n from 'vue-i18n'
+import { messages } from '@/locales/messages'
+import { LoginType } from '@/enum/LoginType';
+import App from '@/App.vue'
 
 const options = {
   url: 'http://192.168.49.17:8080/auth',
@@ -26,4 +34,40 @@ export async function updateToken(){
   }).catch(() => {
     console.log('Failed to refresh token');
   });
+}
+
+export async function initKeycloak(){
+  // Vue.use(Notifications)
+  // Vue.use(VueI18n)
+
+// const i18n = new VueI18n({
+//   locale: "en",
+//   messages: messages
+// })
+
+  keycloak.init({ onLoad: "login-required", checkLoginIframe: false }).then((auth) => {
+    console.log("notdefault")
+    if (!auth) {
+      window.location.reload();
+    } else {
+      // new Vue({
+      //   // i18n,
+      //   router,
+      //   store,
+      //   vuetify,
+      //   render: h => h(App, { props: { keycloak: keycloak, } })
+      // }).$mount('#app');
+      
+      localStorage.setItem('vue-token', keycloak.token!);
+      localStorage.setItem('vue-refresh-token', keycloak.refreshToken!)
+      store.dispatch("chooseLoginType", LoginType.KEYCLOAK);
+    }
+  
+    setInterval(() => { updateToken }, 6000)
+  
+  }).catch((e) => {
+    alert("Login Failure " + e)
+  })
+  
+  Vue.prototype.$keycloak = keycloak
 }
