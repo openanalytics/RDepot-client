fcf95a86f315b172cef7d4ebe5a6f88d6d98210b
kkulesa <katarzyna.kulesa@openanalytics.eu>
2022-11-18 21:30:39 +0100

    add: addSubmission View with Step component

---
+++ src/components/addSubmission/StepFirst.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+    <v-select
+          :items="repositories"
+          :label="$t('addSubmission.step1Title')"
+          item-text="name"
+          dense
+        ></v-select>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props:{
+    step: String
+  },
+  data() {
+    return {
+        repositories: [
+            {
+                id: '1',
+                name: 'repository1'
+            },
+            {
+                id: '2',
+                name: 'repository2'
+            }
+        ]
+    }
+  },
+
+  mounted() {
+  },
+
+  computed: {
+  },
+
+})
+</script>
+
+
