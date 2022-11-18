d75d780fbfe3746c0cc8099b05b805a63eed1218
kkulesa <katarzyna.kulesa@openanalytics.eu>
2022-11-18 20:05:36 +0100

    add: common storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,17 +2,21 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import repositories, { RepositoryState } from "./repositories"
 import users, { UserState } from './users'
+import common, { CommonState } from './common'
 
 Vue.use(Vuex)
 
 export interface State{
+  common: CommonState,
   repositories: RepositoryState,
-  users: UserState
+  users: UserState,
 }
 
 export default new Vuex.Store<State>({
   modules: {
+    common: common,
     repositories: repositories,
     users: users
   }
 })
+
