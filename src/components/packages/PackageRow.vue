523c6f72b4ae8b4b748c27028f577af9acb4bbb4
kkulesa <katarzyna.kulesa@openanalytics.eu>
2022-11-22 14:57:58 +0100

    add: components to work with packages site (not all)

---
+++ src/components/packages/PackageRow.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-row :class="{ title: title }">
+    <v-col cols="1" class="d-flex align-center">{{
+      title == true
+        ? 'Name'
+        : packageBag
+        ? packageBag.name
+        : ''
+    }}</v-col>
+    <v-col cols="1" class="d-flex align-center">{{
+      title == true
+        ? 'Version'
+        : packageBag
+        ? packageBag.version
+        : ''
+    }}</v-col>
+    <v-col cols="6" class="d-flex align-center">
+      {{
+        title == true
+          ? 'Description'
+          : packageBag
+          ? packageBag.desc.length > descMaxLength
+            ? packageBag.desc.slice(0, descMaxLength) +
+              '...'
+            : packageBag.desc
+          : ''
+      }}</v-col
+    >
+    <v-col cols="1" class="d-flex align-center">
+      {{
+        title == true
+          ? 'Maintainer'
+          : packageBag
+          ? packageBag.maintainer
+          : ''
+      }}</v-col
+    >
+    <v-col cols="1" class="d-flex align-center">
+      {{
+        title == true
+          ? 'Repository'
+          : packageBag
+          ? packageBag.reposiotory
+          : ''
+      }}</v-col
+    >
+    <v-col cols="1" class="d-flex justify-center">
+      <span v-if="title == true"> Active </span>
+      <v-checkbox
+        color="text"
+        @click.native.stop
+        v-else-if="packageBag"
+        v-model="packageBag.active"
+      />
+    </v-col>
+    <v-col cls="1" class="d-flex align-center">{{
+      title == true ? 'Actions' : ''
+    }}</v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Package } from '@/models/packages/Package'
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: Boolean,
+      default: false
+    },
+    packageBag: Object as () => Package | undefined
+  },
+  name: 'PackagesRow',
+  data() {
+    return {
+      descMaxLength: 110
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.v-col {
+  padding: 10px !important;
+}
+.col {
+  line-height: 1.3;
+}
+.title {
+  font-weight: 600 !important;
+}
+</style>
---
+++ src/components/packages/PackagesList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-expansion-panels inset>
+      <PackagesListTitle />
+      <PackageItem
+        v-for="(item, index) in packages.data"
+        :key="index"
+        :packageBag="item"
+      />
+    </v-expansion-panels>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import packages from '@/tmpLists/packages.json'
+import PackageItem from './PackageItem.vue'
+import PackagesListTitle from './PackagesListTitle.vue'
+
+export default Vue.extend({
+  name: 'PackagesList',
+  mounted() {
+    this.updateState()
+  },
+  data() {
+    return {
+      packages: packages
+    }
+  },
+  computed: {
+    //uncomment while fetch packages from database
+    //   packages() {
+    //     return this.$store.state.packages.packages
+    // },
+  },
+  methods: {
+    updateState(): void {
+      this.$store.dispatch('fetchPackages')
+    }
+  },
+  components: { PackageItem, PackagesListTitle }
+})
+</script>
