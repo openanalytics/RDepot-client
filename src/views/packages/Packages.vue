22a3abe59d8b1a89ca9823060280c06f48788534
kkulesa <katarzyna.kulesa@openanalytics.eu>
2022-11-22 14:58:08 +0100

    add: view of packages site

---
+++ src/views/packages/Packages.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <PackagesList />
+    {{ page }}
+    <Pagination
+      :howMany="howManyPages"
+      :page="page"
+      v-on:next="nextPage"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import PackagesList from '@/components/packages/PackagesList.vue'
+import Pagination from '@/components/Pagination.vue'
+import store from '@/store'
+
+export default Vue.extend({
+  data() {
+    return {}
+  },
+  computed: {
+    page() {
+      return store.state.packages.page
+    },
+    howManyPages() {
+      return store.state.packages.howManyPages
+    }
+  },
+  components: {
+    PackagesList,
+    Pagination
+  },
+  methods: {
+    nextPage(value: Number) {
+      console.log(value)
+      store.dispatch('setPage', value)
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss"></style>
