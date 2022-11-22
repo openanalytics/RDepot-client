97229808e212643de378ba0a746c35bfc64e4ffb
kkulesa <katarzyna.kulesa@openanalytics.eu>
2022-11-22 11:45:54 +0100

    change: improve submission store

--- src/store/submission.ts
+++ src/store/submission.ts
@@ -1,47 +1,48 @@
+import { Repository } from "@/models";
 import { ActionContext } from "vuex";
 import { State } from ".";
 
 export interface SubmissionState {
-  repositoryId: String,
-  packages: String[]
+  repository: Repository | null,
+  packages: File[]
 }
 
 type Context = ActionContext<SubmissionState, State>;
 
 const submission_state = {
   state: () => ({
-    repositoryId: "0",
+    repository: null,
     packages: []
   }) as SubmissionState,
 
   mutations: {
-    setRepository(state: SubmissionState, payload: String) {
-      state.repositoryId = payload
+    setRepository(state: SubmissionState, payload: Repository) {
+      state.repository = payload
     },
-    setPackages(state: SubmissionState, payload: String[]) {
+    setPackages(state: SubmissionState, payload: File[]) {
       state.packages = payload
     },
-    addPackage(state: SubmissionState, payload: String) {
+    addPackage(state: SubmissionState, payload: File) {
       state.packages = [...state.packages, payload]
     },
-    addPackages(state: SubmissionState, payload: String[]) {
-      payload.forEach((packageBag: String) => {
+    addPackages(state: SubmissionState, payload: File[]) {
+      payload.forEach((packageBag: File) => {
         state.packages = [...state.packages, packageBag]
       })
     }
   },
 
   actions: {
-    setRepository(context: Context, data: String) {
+    setRepository(context: Context, data: Repository) {
       context.commit('setRepository', data)
     },
-    setPackages(context: Context, data: String[]){
+    setPackages(context: Context, data: File[]){
       context.commit("setPackages", data)
     },
-    addPackage(context: Context, data: String){
+    addPackage(context: Context, data: File){
       context.commit("addPackage", data)
     },
-    addPackages(context: Context, data: String[]){
+    addPackages(context: Context, data: File[]){
       context.commit("addPackages", data)
     }
   }
