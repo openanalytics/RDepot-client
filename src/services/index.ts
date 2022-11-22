2e05466baad1f2bee8c3d16e6bae88b867ea7736
kkulesa <katarzyna.kulesa@openanalytics.eu>
2022-11-22 14:58:44 +0100

    add: packages services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,9 @@
 import { fetchRepositoriesServices } from './repository_services'
+import { fetchPackagesServices } from './package_services'
 import { login } from './users_services'
 
 export {
     fetchRepositoriesServices,
+    fetchPackagesServices,
     login
 }
