d27ff36abea548f76f857f5c58caf262bac778df
kkulesa <katarzyna.kulesa@openanalytics.eu>
2022-11-25 12:25:47 +0100

    add: package details empty page and create route to this page

---
+++ src/views/packages/PackageDetails.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    PACKAGE DETAILS
+    <p>
+      {{ name }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    name: String
+  },
+  data() {
+    return {
+      filtrationDialog: false
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss"></style>
