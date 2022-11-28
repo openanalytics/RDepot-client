268dcec07d1868295910818f4e100523d6634e18
kkulesa <katarzyna.kulesa@openanalytics.eu>
2022-11-28 12:42:46 +0100

    change: use routes

--- src/components/navbar/Sidebar.vue
+++ src/components/navbar/Sidebar.vue
@@ -1,9 +1,5 @@
 <template>
-  <v-navigation-drawer
-    permanent
-    :mini-variant.sync="mini"
-    class="sidebar"
-  >
+  <v-navigation-drawer permanent class="sidebar">
     <v-list-item class="itemsList px-2">
       <v-list-item-avatar>
         <v-img
@@ -13,19 +9,19 @@
 
       <v-list-item-title>username</v-list-item-title>
 
-      <v-btn icon @click.stop="mini = !mini">
+      <!-- <v-btn icon @click.stop="mini = !mini">
         <v-icon>mdi-chevron-left</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-list-item>
 
     <v-divider></v-divider>
 
     <v-list nav dense>
       <v-list-item link>
-        <router-link :to="{ name: 'addPackage' }">
-          <v-list-item-icon>
-            <v-icon>mdi-upload</v-icon>
-          </v-list-item-icon>
+        <v-list-item-icon>
+          <v-icon>mdi-upload</v-icon>
+        </v-list-item-icon>
+        <router-link :to="{ name: 'addSubmission' }">
           <v-list-item-title>{{
             $t('common.addPackage')
           }}</v-list-item-title>
@@ -43,15 +39,17 @@
           </v-list-item-title>
         </template>
 
-        <v-list-item>
-          <v-list-item-title
-            class="pl-15"
-            v-text="$t('common.list')"
-          ></v-list-item-title>
-          <v-list-item-icon>
-            <v-icon>mdi-format-list-bulleted</v-icon>
-          </v-list-item-icon>
-        </v-list-item>
+        <router-link :to="{ name: 'packages' }">
+          <v-list-item>
+            <v-list-item-title
+              class="pl-15"
+              v-text="$t('common.list')"
+            ></v-list-item-title>
+            <v-list-item-icon>
+              <v-icon>mdi-format-list-bulleted</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </router-link>
         <v-list-item>
           <v-list-item-title
             class="pl-15"
@@ -123,10 +121,17 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 .sidebar {
-  position: absolute;
   max-height: 100%;
+  width: 100%;
   .itemsList {
     margin-top: 80px;
   }
+}
+
+// .sidebar-hide {
+//   min-width: 60px;
+// }
+.sidebar-show {
+  min-width: 350px;
 }
 </style>
