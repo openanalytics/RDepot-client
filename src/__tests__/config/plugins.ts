f546409e5d8ebe65678de46d1fccb12be5e5c2b6
kkulesa <katarzyna.kulesa@openanalytics.eu>
2023-01-17 12:50:43 +0100

    change: tests configuration

--- src/__tests__/config/plugins.ts
+++ src/__tests__/config/plugins.ts
@@ -1,10 +1,11 @@
 import { createVuetify } from 'vuetify/lib/framework.mjs'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import Notifications from '@kyvg/vue3-notification'
 
 const vuetify = createVuetify({
   components,
   directives
 })
 
-export const plugins = [vuetify]
+export const plugins = [vuetify, Notifications]
