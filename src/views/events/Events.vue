460c150e10343356be92545edb40ff3ed69320a0
kkulesa <katarzyna.kulesa@openanalytics.eu>
2023-02-08 17:14:34 +0100

    add: events view

---
+++ src/views/events/Events.vue
@@ -0,0 +1,10 @@
+import EventsTimeLine from
+'@/components/events/EventsTimeLine.vue';
+
+<template>
+  <EventsTimeLine></EventsTimeLine>
+</template>
+
+<script setup lang="ts">
+import EventsTimeLine from '@/components/events/EventsTimeLine.vue'
+</script>
