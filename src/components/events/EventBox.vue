78ad44834e4840663449885fe0ae4de1f38ef554
kkulesa <katarzyna.kulesa@openanalytics.eu>
2023-02-08 17:14:10 +0100

    add: component for single event - to discuss how it should look like

---
+++ src/components/events/EventBox.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card>
+    <!-- <v-card-title
+          :class="['text-h6', `bg-${item.color}`]"
+        >
+          Lorem Ipsum Dolor
+        </v-card-title> -->
+    <v-card-text class="bg-white text--primary">
+      <p>
+        {{ props.event?.resourceDescription }}
+        {{ props.event?.time }}
+        {{ props.event?.user }}
+      </p>
+      <!-- :color="item.color" -->
+      <v-btn variant="outlined">
+        {{ props.event?.eventType }}
+      </v-btn>
+      <v-btn variant="outlined" v-if="props.event?.time">
+        {{ date?.getDay() }}
+        {{ date?.getDate() }}
+        {{ date?.getMonth() }}
+        {{ date?.getFullYear() }}
+      </v-btn>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { EntityModelNewsfeedEventDto } from '@/openapi'
+import { computed } from 'vue'
+const props = defineProps({
+  event: Object as () => EntityModelNewsfeedEventDto
+})
+const date = computed(function () {
+  if (props.event?.time) {
+    return new Date(props.event.time)
+  } else {
+    return null
+  }
+})
+</script>
