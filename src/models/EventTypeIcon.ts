f8ae9c545f0340a39db7a6f5f7e0ff7c3ae26915
kkulesa <katarzyna.kulesa@openanalytics.eu>
2023-02-08 17:13:32 +0100

    add: map between event type and icon

---
+++ src/models/EventTypeIcon.ts
@@ -0,0 +1,10 @@
+import { EntityModelNewsfeedEventDtoResourceTypeEnum } from '@/openapi'
+
+export let eventsIcons = new Map<string, string>([
+  ['update', 'mdi-check'],
+  ['removed', 'mdi-delete-forever'],
+  ['submitted', 'mdi-cloud-upload'],
+  ['create', 'mdi-cake-variant'],
+  ['edited', 'mdi-lead-pencil'],
+  ['delete', 'mdi-delete-forever']
+])
