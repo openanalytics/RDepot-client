5f3d8aa268c2e84c666e132d4c43d3320c6f3746
kkulesa <katarzyna.kulesa@openanalytics.eu>
2023-02-13 14:05:59 +0100

    change:  update event box

--- src/components/events/EventBox.vue
+++ src/components/events/EventBox.vue
@@ -1,16 +1,27 @@
 <template>
-  <v-card>
-    <!-- <v-card-title
-          :class="['text-h6', `bg-${item.color}`]"
-        >
-          Lorem Ipsum Dolor
-        </v-card-title> -->
-    <v-card-text class="bg-white text--primary">
-      <p>
-        {{ props.event?.resourceDescription }}
-        {{ props.event?.time }}
-        {{ props.event?.user }}
-      </p>
+  <v-card class="eventCard">
+    <v-card-title
+      style="padding: 0"
+      class="d-flex justify-space-between align-items-start"
+    >
+      <div class="title">
+        {{
+          props.event?.resourceType?.replaceAll('_', ' ')
+        }}
+      </div>
+      <div class="date">
+        {{ shortDate }}
+      </div>
+    </v-card-title>
+    <v-card-text class="eventCard">
+      <div class="d-flex justify-beteen align-items-start">
+        <div class="description">
+          {{ props.event?.resourceDescription }}
+          {{ props.event?.time }}
+          {{ props.event?.user }}
+        </div>
+      </div>
+      <p></p>
       <!-- :color="item.color" -->
       <v-btn variant="outlined">
         {{ props.event?.eventType }}
@@ -38,4 +49,37 @@ const date = computed(function () {
     return null
   }
 })
+
+const shortDate = computed(function () {
+  if (props.event?.time) {
+    let date: string = new Date(props.event.time).toString()
+    return date.substring(4, date.indexOf(' GMT'))
+  } else {
+    return null
+  }
+})
 </script>
+
+<style lang="scss">
+.eventCard {
+  max-width: 500px;
+
+  .title {
+    padding: 0.5em;
+  }
+
+  .date {
+    font-size: 12px;
+    border-left: solid rgb(var(--v-theme-oablue)) 1px;
+    border-bottom: solid rgb(var(--v-theme-oablue)) 1px;
+    border-bottom-left-radius: 16px;
+    padding: 4px 10px;
+    // line-height: 1;
+    max-height: 40px;
+  }
+
+  .description {
+    margin-top: 10px;
+  }
+}
+</style>
