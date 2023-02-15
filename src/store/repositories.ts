729f0c838c2a50091f203d64deb81dab3e99d424
kkulesa <katarzyna.kulesa@openanalytics.eu>
2023-02-15 18:54:42 +0100

    add: repository store

--- src/store/repositories.ts
+++ src/store/repositories.ts
@@ -1,8 +1,11 @@
-import { Repository } from '@/models/repositories/Repository'
+import { EntityModelRRepositoryDto } from '@/openapi'
 import { defineStore } from 'pinia'
+import repositories from '@/tmpLists/repositories.json'
+import { RepositoriesFiltration } from '@/models/Filtration'
 
 interface State {
-  repositories: Repository[]
+  repositories: EntityModelRRepositoryDto[]
+  filtration: RepositoriesFiltration
 }
 
 export const useRepositoryStore = defineStore(
@@ -10,13 +13,38 @@ export const useRepositoryStore = defineStore(
   {
     state: (): State => {
       return {
-        repositories: []
+        repositories: [],
+        filtration: {
+          name: {
+            label: 'Name',
+            requestName: 'name',
+            value: ''
+          },
+          technology: {
+            label: 'Technology',
+            requestName: 'technology',
+            value: ''
+          }
+        }
       }
     },
     actions: {
       async fetchRepositories() {
-        // let repositories = await fetchRepositoriesServices()
-        // this.repositories = repositories
+        this.repositories = JSON.parse(
+          JSON.stringify(repositories.data)
+        )
+      },
+      async setFiltration(payload: RepositoriesFiltration) {
+        this.filtration = payload
+        this.fetchRepositories()
+      },
+      clearFiltration() {
+        this.filtration.technology.value = ''
+        this.filtration.name.value = ''
+      },
+      async clearFiltrationAndFetch() {
+        this.clearFiltration()
+        this.fetchRepositories()
       }
     }
   }
