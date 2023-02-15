f2b7e44a3a0da35d6b337f4b9550f09a6ea9ab90
kkulesa <katarzyna.kulesa@openanalytics.eu>
2023-02-15 18:54:04 +0100

    add: view for repositories with filtration and list

--- src/views/repositories/Repositories.vue
+++ src/views/repositories/Repositories.vue
@@ -1,16 +1,36 @@
 <template>
-  <div>
-    {{ title }}
-    {{ repositories }}
-  </div>
+  <v-row justify="end" class="my-5 mx-10" align="center">
+    <CommonButton
+      id="reset-packages-filtration"
+      :title="$t('common.reset')"
+      v-on:buttonClicked="
+        showOverlay(OverlayEnum.RepositoryFiltrationReset)
+      "
+      class="mx-3"
+    />
+    <CommonButton
+      :title="$t('filtration.title')"
+      v-on:buttonClicked="
+        showOverlay(OverlayEnum.RepositoryFiltration)
+      "
+      class="mx-3"
+    />
+  </v-row>
+  <RepositoriesList />
 </template>
 
 <script setup lang="ts">
+import RepositoriesList from '@/components/repositories/RepositoriesList.vue'
+import CommonButton from '@/components/common/Button.vue'
+import { OverlayEnum } from '@/enum/Overlay'
+import { useCommonStore } from '@/store/common'
 import { useRepositoryStore } from '@/store/repositories'
 import { computed, onMounted, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
 
-const title = ref('repositories')
+const common_store = useCommonStore()
 const repository_store = useRepositoryStore()
+const { t } = useI18n()
 
 const repositories = computed(function () {
   return repository_store.repositories
@@ -23,4 +43,11 @@ function updateState() {
 onMounted(() => {
   updateState()
 })
+
+function showOverlay(value: number) {
+  common_store.setOverlayText(t('filtration.makeSure'))
+  common_store.setOverlayModel(true)
+  common_store.setOverlayOpacity(0.8)
+  common_store.setOverlayComponent(value)
+}
 </script>
