93ad98b1fdc02d17c1683a0b0243dbbd67c6427a
kkulesa <katarzyna.kulesa@openanalytics.eu>
2023-02-15 16:21:58 +0100

    add: overlay store in commonstore so can be use by each component

--- src/store/common.ts
+++ src/store/common.ts
@@ -3,13 +3,21 @@ import { defineStore } from 'pinia'
 interface State {
   drawer: boolean
   progressCircularActive: boolean
+  overlayText: string
+  overlayModel: boolean
+  overlayOpacity: number
+  overlayComponent: number
 }
 
 export const useCommonStore = defineStore('common_store', {
   state: (): State => {
     return {
       drawer: true,
-      progressCircularActive: false
+      progressCircularActive: false,
+      overlayText: '',
+      overlayOpacity: 0.8,
+      overlayModel: false,
+      overlayComponent: 0
     }
   },
   actions: {
@@ -18,6 +26,18 @@ export const useCommonStore = defineStore('common_store', {
     },
     async setDrawer(payload: boolean) {
       this.drawer = payload
+    },
+    setOverlayText(payload: string) {
+      this.overlayText = payload
+    },
+    setOverlayModel(payload: boolean) {
+      this.overlayModel = payload
+    },
+    setOverlayOpacity(payload: number) {
+      this.overlayOpacity = payload
+    },
+    setOverlayComponent(payload: number) {
+      this.overlayComponent = payload
     }
   }
 })
