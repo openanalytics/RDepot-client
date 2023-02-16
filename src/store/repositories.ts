8a8f6380fc2021ebbc1b761590ba32fc75249bd4
kkulesa <katarzyna.kulesa@openanalytics.eu>
2023-02-16 09:43:03 +0100

    update: reposiotry store with choosen one (choosen to see details)

--- src/store/repositories.ts
+++ src/store/repositories.ts
@@ -1,11 +1,17 @@
-import { EntityModelRRepositoryDto } from '@/openapi'
+import {
+  EntityModelPackageDtoObjectObject,
+  EntityModelRRepositoryDto
+} from '@/openapi'
 import { defineStore } from 'pinia'
 import repositories from '@/tmpLists/repositories.json'
+import packages from '@/tmpLists/packages.json'
 import { RepositoriesFiltration } from '@/models/Filtration'
 
 interface State {
   repositories: EntityModelRRepositoryDto[]
   filtration: RepositoriesFiltration
+  choosenRepository: number
+  repositoryPackages: EntityModelPackageDtoObjectObject[]
 }
 
 export const useRepositoryStore = defineStore(
@@ -25,13 +31,20 @@ export const useRepositoryStore = defineStore(
             requestName: 'technology',
             value: ''
           }
-        }
+        },
+        choosenRepository: -1,
+        repositoryPackages: []
       }
     },
     actions: {
       async fetchRepositories() {
         this.repositories = JSON.parse(
           JSON.stringify(repositories.data)
+        )
+      },
+      async fetchPackages() {
+        this.repositoryPackages = JSON.parse(
+          JSON.stringify(packages.page2)
         )
       },
       async setFiltration(payload: RepositoriesFiltration) {
