b2748f917f267915ef23f9a852afb88d9734bb85
kkulesa <katarzyna.kulesa@openalytics.eu>
2023-02-22 15:50:49 +0100

    add: submission filtration + simulated delay

--- src/store/submission.ts
+++ src/store/submission.ts
@@ -2,14 +2,17 @@ import { Repository } from '@/models'
 import { EntityModelSubmissionDto } from '@/openapi'
 import { defineStore } from 'pinia'
 import submissions from '@/tmpLists/rSubmissions.json'
+import { SubmissionsFiltration } from '@/models/Filtration'
+import { notify } from '@kyvg/vue3-notification'
+import { i18n } from '@/plugins/i18n'
 
 interface State {
   repository: Repository | null
   packages: File[]
   submissions: EntityModelSubmissionDto[]
+  filtration: SubmissionsFiltration
 }
 
-export const useSubmissionState = defineStore(
 export const useSubmissionStore = defineStore(
   'submission_store',
   {
@@ -17,11 +20,21 @@ export const useSubmissionStore = defineStore(
       return {
         repository: null,
         packages: [],
-        submissions: []
+        submissions: [],
+        filtration: {
+          package: '',
+          state: '',
+          assignedToMe: false
+        }
       }
     },
     actions: {
-      fetchSubmissions() {
+      timeout(ms: number) {
+        return new Promise((resolve) =>
+          setTimeout(resolve, ms)
+        )
+      },
+      async fetchSubmissions() {
         this.submissions = JSON.parse(
           JSON.stringify(submissions.content)
         )
@@ -38,6 +51,31 @@ export const useSubmissionStore = defineStore(
       addPackages(payload: File[]) {
         payload.forEach((packageBag: File) => {
           this.packages = [...this.packages, packageBag]
+        })
+      },
+      async setFiltration(payload: SubmissionsFiltration) {
+        this.filtration = payload
+        await this.fetchSubmissions()
+        await this.timeout(5000)
+        notify({
+          text: i18n.t('notifications.successFiltration'),
+          type: 'success'
+        })
+      },
+      clearFiltration() {
+        this.filtration.state = ''
+        this.filtration.assignedToMe = false
+        this.filtration.package = ''
+      },
+      async clearFiltrationAndFetch() {
+        this.clearFiltration()
+        await this.fetchSubmissions()
+        await this.timeout(5000)
+        notify({
+          text: i18n.t(
+            'notifications.successFiltrationReset'
+          ),
+          type: 'success'
         })
       }
     }
