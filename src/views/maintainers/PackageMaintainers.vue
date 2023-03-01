c8af657b2a1d9f3efc0a114757ca1017b66bab0b
kkulesa <katarzyna.kulesa@openalytics.eu>
2023-03-01 20:19:46 +0100

    add: package maitainers view (without filtration and modals)

---
+++ src/views/maintainers/PackageMaintainers.vue
@@ -0,0 +1,11 @@
+<template>
+  <RepositoriesModal />
+  <RepositoriesButtons />
+  <PackageMaintainersList />
+</template>
+
+<script setup lang="ts">
+import RepositoriesModal from '@/components/repositories/RepositoriesModal.vue'
+import RepositoriesButtons from '@/components/repositories/RepositoriesButtons.vue'
+import PackageMaintainersList from '@/components/maintainers/PackageMaintainersList.vue'
+</script>
