26c7961dee9d10ceb6deb210325022d3bad54129
kkulesa <katarzyna.kulesa@openalytics.eu>
2023-03-05 13:18:46 +0100

    update: store with filtration and fetchnig repositories/packages connected to package maintainer edition

--- src/store/package_maintainers.ts
+++ src/store/package_maintainers.ts
@@ -1,11 +1,20 @@
-import { EntityModelPackageMaintainerDto } from '@/openapi'
+import {
+  EntityModelPackageMaintainerDto,
+  EntityModelPythonRepositoryDto,
+  EntityModelRPackageDto
+} from '@/openapi'
 import { defineStore } from 'pinia'
 import package_maintainers from '@/tmpLists/packageMaintainers.json'
+import repositories from '@/tmpLists/repositories.json'
+import packages from '@/tmpLists/packages.json'
 import { MaintainersFiltration } from '@/models/Filtration'
 
 interface State {
   maintainers: EntityModelPackageMaintainerDto[]
   filtration: MaintainersFiltration
+  repositories: EntityModelPythonRepositoryDto[]
+  packages: EntityModelRPackageDto[]
+  choosenMaintainer: EntityModelPackageMaintainerDto
 }
 
 export const usePackageMaintainersStore = defineStore(
@@ -17,13 +26,45 @@ export const usePackageMaintainersStore = defineStore(
         filtration: {
           deleted: false,
           technology: ''
-        }
+        },
+        repositories: [],
+        packages: [],
+        choosenMaintainer: {}
       }
     },
     actions: {
       async fetchMaintainers() {
         this.maintainers = JSON.parse(
           JSON.stringify(package_maintainers.data)
+        )
+      },
+      async fetchReposiotires() {
+        this.repositories = JSON.parse(
+          JSON.stringify(repositories.data)
+        )
+      },
+      async fetchPackages() {
+        this.packages = JSON.parse(
+          JSON.stringify(packages.page1)
+        ).concat(JSON.parse(JSON.stringify(packages.page2)))
+      },
+
+      async setChoosenMaintainer(
+        payload: EntityModelPackageMaintainerDto
+      ) {
+        this.choosenMaintainer = payload
+        this.saveMaintainer()
+      },
+      async saveMaintainer() {
+        this.maintainers = this.maintainers.map(
+          (maintainer: EntityModelPackageMaintainerDto) => {
+            if (
+              maintainer.id == this.choosenMaintainer.id
+            ) {
+              return this.choosenMaintainer
+            }
+            return maintainer
+          }
         )
       },
       async setFiltration(payload: MaintainersFiltration) {
