7d8f640c48eadba19d2927a3331268c402463022
kkulesa <katarzyna.kulesa@openalytics.eu>
2023-03-06 13:12:09 +0100

    add: view with repository maintainers

--- src/views/maintainers/RepositoryMaintainers.vue
+++ src/views/maintainers/RepositoryMaintainers.vue
@@ -1,2 +1,11 @@
-<template>raz dwa</template>
-<script setup lang="ts"></script>
+<template>
+  <MaintainersModal />
+  <MaintainersButtons />
+  <RepositoryMaintainersList />
+</template>
+
+<script setup lang="ts">
+import MaintainersButtons from '@/components/repositoryMaintainers/RepositoryMaintainersButtons.vue'
+import RepositoryMaintainersList from '@/components/repositoryMaintainers/RepositoryMaintainersList.vue'
+import MaintainersModal from '@/components/repositoryMaintainers/RepositoryMaintainersModal.vue'
+</script>
