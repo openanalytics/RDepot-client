e714c844f0de28130cc0080b8306dcfea51e48f7
kkulesa <katarzyna.kulesa@openalytics.eu>
2023-03-10 13:38:34 +0100

    add: common filtration buttons that can be used by each list

---
+++ src/components/common/FiltrationButtons.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row justify="end" class="my-5 mx-10" align="center">
+    <CommonButton
+      id="reset-maintainers-filtration"
+      :component="OverlayEnum.Reset"
+      >{{ $t('common.reset') }}</CommonButton
+    >
+    <CommonButton :component="OverlayEnum.Filtration" />
+  </v-row>
+</template>
+
+<script setup lang="ts">
+import CommonButton from '@/components/common/Button.vue'
+import { OverlayEnum } from '@/enum/Overlay'
+</script>
