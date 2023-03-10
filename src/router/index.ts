e0c4a3b90a71353af64dd7cb699c967f1b108421
kkulesa <katarzyna.kulesa@openalytics.eu>
2023-03-10 13:43:56 +0100

    add: title of page before each route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,20 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from '@/router/routes'
+import { i18n } from '@/plugins/i18n'
 
-const DEFAULT_TITLE = 'RDepot'
+const DEFAULT_TITLE = i18n.t('common.projectTitle')
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+    ? (to.meta.title as string)
+    : DEFAULT_TITLE
+  next()
 })
 
 export default router
