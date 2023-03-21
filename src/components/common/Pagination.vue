d084dde42666c4f8055ebe090bce0f933e941776
kkulesa <katarzyna.kulesa@openalytics.eu>
2023-03-21 14:21:47 +0100

    update: paginantion

--- src/components/common/Pagination.vue
+++ src/components/common/Pagination.vue
@@ -17,11 +17,14 @@
               ></v-pagination>
               <v-row class="pageSize">
                 <v-text-field
+                  id="page-size-input"
+                  ref="pageSizeInput"
                   style="flex: 1"
                   width="40"
                   v-model="pageSize"
                   type="number"
                   color="text"
+                  aria-valuemin="1"
                   :label="$t('pagination.size')"
                 ></v-text-field>
               </v-row>
@@ -35,34 +38,45 @@
 
 <script setup lang="ts">
 import { usePackagesStore } from '@/store/packages'
-import { ref } from 'vue'
-import { computed } from '@vue/runtime-core'
+import { computed, ref } from 'vue'
 
-var props = defineProps({
-  howMany: Number,
-  page: Number
-})
-
-const emit = defineEmits(['newPage'])
-const allPackages = ref(100)
+const emit = defineEmits(['newPage', 'newPageSize'])
 const package_store = usePackagesStore()
 
+const pageSizeInput = ref<HTMLDivElement>()
+
 const howManyPages = computed(function () {
-  return allPackages.value / package_store.pageSize
+  if (package_store.totalNumber && package_store.pageSize) {
+    return Math.ceil(
+      package_store.totalNumber / package_store.pageSize
+    )
+  }
+  // return 2
 })
 
 const pageSize = computed({
   get() {
     return package_store.pageSize
   },
-  set(value: number) {
-    package_store.setPageSize(value)
+  set: (value) => {
+    if (pageSizeInput.value && pageSizeInput)
+      console.log(pageSizeInput.value.hasAttribute('focus'))
+
+    if (
+      pageSizeInput.value &&
+      !pageSizeInput.value.hasAttribute('focus')
+    )
+      emit('newPageSize', value)
   }
 })
 
 const page = computed({
-  get: () => props.page,
-  set: (value) => emit('newPage', value)
+  get: () => {
+    if (package_store.page) return package_store.page + 1
+  },
+  set: (value) => {
+    if (value) emit('newPage', value - 1)
+  }
 })
 </script>
 
