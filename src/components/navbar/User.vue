7068207ef9271dc84e1092524da0ff24c2d07da9
kkulesa <katarzyna.kulesa@openalytics.eu>
2023-03-21 14:22:52 +0100

    add: user component (developers only)

---
+++ src/components/navbar/User.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-menu offset-y>
+    <template v-slot:activator="{ props }">
+      <v-btn
+        id="change-user-button"
+        color="oared"
+        variant="elevated"
+        v-bind="props"
+        class="px-3 custom-button mx-3"
+        depressed
+      >
+        {{ user.login }} ({{ user.role }})
+      </v-btn>
+    </template>
+    <v-list>
+      <v-list-item
+        v-for="(item, index) in users"
+        :key="index"
+        @click="changeUser(item)"
+        link
+      >
+        <v-list-item-title
+          v-text="item.login + ' (' + item.role + ')'"
+        ></v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useLoggedUserStore } from '@/store/logged_user'
+import router from '@/router'
+import { useCommonStore } from '@/store/common'
+const users = [
+  {
+    token: import.meta.env.VITE_ADMIN_TOKEN,
+    login: 'einstein',
+    role: 'admin'
+  },
+  {
+    token: import.meta.env.VITE_REPOSITORY_MAINTAINER_TOKEN,
+    login: 'tesla',
+    role: 'repository maintainer'
+  },
+  {
+    token: import.meta.env.VITE_PACKAGE_MAINTAINER_TOKEN,
+    login: 'galileo',
+    role: 'package maintainer'
+  },
+  {
+    token: import.meta.env.VITE_USER_TOKEN,
+    login: 'newton',
+    role: 'user'
+  }
+]
+const common_store = useCommonStore()
+const logged_user_store = useLoggedUserStore()
+const user = ref({
+  token: logged_user_store.userToken,
+  login: logged_user_store.userLogin,
+  role: logged_user_store.userRole
+})
+function changeUser(new_user: any) {
+  user.value = new_user
+  logged_user_store.change_user(
+    user.value.token,
+    user.value.login,
+    user.value.role
+  )
+  common_store.updateKey()
+}
+</script>
