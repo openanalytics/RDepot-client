3f8a00b8edbfab384d2926853631f354e09b8e48
kkulesa <katarzyna.kulesa@openalytics.eu>
2023-03-23 08:14:57 +0100

    fix: add userId

--- src/components/navbar/User.vue
+++ src/components/navbar/User.vue
@@ -29,28 +29,31 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useLoggedUserStore } from '@/store/logged_user'
-import router from '@/router'
 import { useCommonStore } from '@/store/common'
 const users = [
   {
     token: import.meta.env.VITE_ADMIN_TOKEN,
     login: 'einstein',
-    role: 'admin'
+    role: 'admin',
+    id: 8
   },
   {
     token: import.meta.env.VITE_REPOSITORY_MAINTAINER_TOKEN,
     login: 'tesla',
-    role: 'repository maintainer'
+    role: 'repository maintainer',
+    id: 5
   },
   {
     token: import.meta.env.VITE_PACKAGE_MAINTAINER_TOKEN,
     login: 'galileo',
-    role: 'package maintainer'
+    role: 'package maintainer',
+    id: 6
   },
   {
     token: import.meta.env.VITE_USER_TOKEN,
     login: 'newton',
-    role: 'user'
+    role: 'user',
+    id: 7
   }
 ]
 const common_store = useCommonStore()
@@ -58,14 +61,16 @@ const logged_user_store = useLoggedUserStore()
 const user = ref({
   token: logged_user_store.userToken,
   login: logged_user_store.userLogin,
-  role: logged_user_store.userRole
+  role: logged_user_store.userRole,
+  id: logged_user_store.userId
 })
 function changeUser(new_user: any) {
   user.value = new_user
   logged_user_store.change_user(
     user.value.token,
     user.value.login,
-    user.value.role
+    user.value.role,
+    user.value.id
   )
   common_store.updateKey()
 }
--- src/store/logged_user.ts
+++ src/store/logged_user.ts
@@ -4,6 +4,7 @@ interface State {
   userToken: string
   userLogin: string
   userRole: string
+  userId: number
 }
 
 export const useLoggedUserStore = defineStore(
@@ -13,18 +14,21 @@ export const useLoggedUserStore = defineStore(
       return {
         userToken: import.meta.env.VITE_ADMIN_TOKEN,
         userLogin: 'einstein',
-        userRole: 'admin'
+        userRole: 'admin',
+        userId: 8
       }
     },
     actions: {
       change_user(
         token: string,
         login: string,
-        role: string
+        role: string,
+        id: number
       ) {
         this.userToken = token
         this.userLogin = login
         this.userRole = role
+        this.userId = id
       }
     }
   }
