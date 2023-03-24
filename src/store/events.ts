116da7b323e7178d08881b3a843c156dc6c9f294
kkulesa <katarzyna.kulesa@openalytics.eu>
2023-03-24 13:04:51 +0100

    feat: use service to get events, add possibility to fetch events page by page with concatening

--- src/store/events.ts
+++ src/store/events.ts
@@ -1,47 +1,67 @@
-import {
-  ApiV2NewsfeedEventControllerApiFp,
-  EntityModelNewsfeedEventDto
-} from '@/openapi'
-import events from '@/tmpLists/events.json'
-import eventsNext from '@/tmpLists/eventsNext.json'
+import { EntityModelNewsfeedEventDto } from '@/openapi'
 import { defineStore } from 'pinia'
 import { EventsFiltration } from '@/models/Filtration'
+import { fetchEventsServices } from '@/services/events_services'
+import { notify } from '@kyvg/vue3-notification'
 
 interface State {
-  page: number
-  events: EntityModelNewsfeedEventDto[]
+  page?: number
+  pageSize: number
+  totalNumber?: number
+  events?: EntityModelNewsfeedEventDto[]
   filtration: EventsFiltration
 }
 
 export const useEventsStore = defineStore('events_store', {
   state: (): State => {
     return {
-      page: 1,
-      events: JSON.parse(JSON.stringify(events.events)),
+      page: 0,
+      pageSize: 10,
+      totalNumber: 0,
+      events: [],
       filtration: {
-        eventType: '',
+        eventType: undefined,
         resourceId: undefined,
-        resourceType: '',
-        technology: '',
+        resourceType: undefined,
+        technology: undefined,
         userId: undefined
       }
     }
   },
   actions: {
     async fetchEvents() {
-      var tmpEvents =
-        ApiV2NewsfeedEventControllerApiFp().getAllEvents()
-      console.log(tmpEvents)
-
-      this.events = JSON.parse(
-        JSON.stringify(events.events)
+      fetchEventsServices(
+        this.filtration,
+        this.page,
+        this.pageSize
+      ).then(
+        (res) => {
+          this.page = res.data.data?.page?.number
+          this.totalNumber =
+            res.data.data?.page?.totalElements
+          if (
+            this.events != undefined &&
+            res.data.data?.content
+          ) {
+            this.events = this.events.concat(
+              res.data.data?.content
+            )
+          } else {
+            this.events = res.data.data?.content
+          }
+        },
+        (msg) => {
+          this.events = []
+          this.page = 0
+          notify({ text: msg, type: 'error' })
+        }
       )
     },
     async fetchNextPageEvents() {
-      this.page = this.page + 1
-      this.events = this.events.concat(
-        JSON.parse(JSON.stringify(eventsNext.events))
-      )
+      if (this.page != undefined) {
+        this.page = this.page + 1
+        this.fetchEvents()
+      }
     },
     async setPage(payload: number) {
       this.page = payload
