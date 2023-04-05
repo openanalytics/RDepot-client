150c30d4a7a6de23d15b7315801997570bdc10b2
Lucianos Lionakis <lucianos.lionakis@openanalytics.eu>
2023-04-05 09:37:41 +0200

    Add: ux for the third step of adding submissions

--- src/components/addSubmission/StepThird.vue
+++ src/components/addSubmission/StepThird.vue
@@ -1,27 +1,30 @@
 <template>
-  <v-card class="mb-12 px-10 py-5 step" height="250px">
-    <div class="ml-4">{{ choosenRepository }}</div>
-    <div class="ml-4">
-      Accepted packages index: {{ accepted_packages }}
-    </div>
-    <div class="ml-4">
-      Choosen packages index:
-      {{ choosenPackages.map((s, i) => i) }}
-    </div>
+  <v-card class="mb-12 px-10 py-3 step text-center">
+    <v-card-text class="mb-1">
+      <div class="text-overline">repository</div>
+      <div class="text-h4 mb-2">
+        {{ choosenRepository?.name }}
+      </div>
+      <v-divider></v-divider>
+      <div class="text-overline">packages</div>
+      <v-list class="text-left">
+        <v-list-item
+          v-for="(file, i) in choosenPackages"
+          :key="i"
+          class="justify-start hoverable"
+        >
+          <template v-slot:prepend>
+            <v-icon color="white" icon="mdi-file" />
+          </template>
 
-    <v-chip-group
-      v-model="accepted_packages"
-      column
-      multiple
-    >
-      <v-chip
-        v-for="(file, index) in choosenPackages"
-        :key="index"
-        filter
-        variant="outlined"
-        >{{ file.name }}
-      </v-chip>
-    </v-chip-group>
+          <template v-slot:append>
+            <v-list-item-title>
+              {{ file.name }}
+            </v-list-item-title>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
   </v-card>
   <div class="d-flex justify-space-between">
     <v-btn id="backbutton" color="oablue" @click="backStep">
@@ -62,3 +65,12 @@ async function submit() {
   await submissions_store.addSumbissionRequests()
 }
 </script>
+
+<style lang="scss">
+.hoverable {
+  transition: background-color 0.5s ease;
+  &:hover {
+    background-color: darkslategray;
+  }
+}
+</style>
