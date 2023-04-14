02ec38c57563d34bdca7d2c3435d94f244b60c5d
kkulesa <katarzyna.kulesa@openalytics.eu>
2023-04-14 16:28:15 +0200

    add: component for each text displayed in tables

---
+++ src/components/packages/TextRecord.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="mr-8"> {{ prepareText() }} </span>
+</template>
+
+<script setup lang="ts">
+import { ref } from '@vue/reactivity'
+
+const props = defineProps({
+  text: {
+    type: String
+  }
+})
+
+const descMaxLength = ref(110)
+
+function prepareText(): string {
+  var text: string | undefined = props.text
+  if (text) {
+    if (text.length > descMaxLength.value) {
+      text.slice(0, descMaxLength.value) + '...'
+    }
+    return text
+  }
+  return ''
+}
+</script>
