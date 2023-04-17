df299b080d4aa14d0eea7062564e6833af139911
Lucianos Lionakis <lucianos.lionakis@openanalytics.eu>
2023-04-17 11:01:43 +0200

    add: add repository creation function to repository services

--- src/services/repository_services.ts
+++ src/services/repository_services.ts
@@ -1,12 +1,19 @@
 import { RepositoriesFiltration } from '@/models/Filtration'
 import {
   ApiV2RepositoryControllerApiFactory,
+  EntityModelRepositoryDto,
+  PythonRepositoryControllerApiFactory,
+  PythonRepositoryDto,
   ResponseDtoPagedModelEntityModelRepositoryDto,
   ResponseDtoPagedModelEntityModelRRepositoryDto,
-  RRepositoryControllerApiFactory
+  RRepositoryControllerApiFactory,
+  RRepositoryDto
 } from '@/openapi'
 import { getConfiguration } from './api_config'
 import { openApiRequest } from './open_api_access'
+import { TechnologiesEnum } from '@/enum/Technologies'
+import { notify } from '@kyvg/vue3-notification'
+import { i18n } from '@/plugins/i18n'
 
 export function fetchRepositoriesServices(
   filtration?: RepositoriesFiltration,
@@ -34,4 +41,65 @@ export function fetchRRepositories() {
   return openApiRequest<ResponseDtoPagedModelEntityModelRRepositoryDto>(
     r_repository_api.getAllRRepositories
   )
+}
+
+export function createRepository(
+  newRepository: EntityModelRepositoryDto
+) {
+  if (newRepository.technology === TechnologiesEnum.R) {
+    const repository_api = RRepositoryControllerApiFactory(
+      getConfiguration()
+    )
+    return openApiRequest<ResponseDtoPagedModelEntityModelRepositoryDto>(
+      repository_api.createRRepository,
+      [newRepository as RRepositoryDto]
+    ).then(
+      () => {
+        notify({
+          type: 'success',
+          text: i18n.t(
+            'notifications.successCreateRepository',
+            newRepository.name || ''
+          )
+        })
+        return true
+      },
+      (msg) => {
+        notify({ type: 'error', text: msg })
+        return false
+      }
+    )
+  } else if (
+    newRepository.technology === TechnologiesEnum.Python
+  ) {
+    const repository_api =
+      PythonRepositoryControllerApiFactory(
+        getConfiguration()
+      )
+    return openApiRequest<ResponseDtoPagedModelEntityModelRepositoryDto>(
+      repository_api.createPythonRepository,
+      [newRepository as PythonRepositoryDto]
+    ).then(
+      () => {
+        notify({
+          type: 'success',
+          text: i18n.t(
+            'notifications.successCreateRepository',
+            newRepository.name || ''
+          )
+        })
+        return true
+      },
+      (msg) => {
+        notify({ type: 'error', text: msg })
+        return false
+      }
+    )
+  } else {
+    notify({
+      type: 'error',
+      text: `Invalid technology '${newRepository.technology}'`
+    })
+    return new Promise<boolean>(() => false)
+  }
 }
