ca5799d7b552b5aaa6776da50403962d4ab2be30
Lucianos Lionakis <lucianos.lionakis@openanalytics.eu>
2023-04-17 10:54:43 +0200

    add: common creation button

---
+++ src/components/common/AddButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <CommonButton
+    id="add-maintainer"
+    :component="OverlayEnum.Create"
+    ><v-icon icon="mdi-plus"
+  /></CommonButton>
+  <v-spacer></v-spacer>
+</template>
+
+<script setup lang="ts">
+import CommonButton from '@/components/common/Button.vue'
+import { OverlayEnum } from '@/enum/Overlay'
+</script>
