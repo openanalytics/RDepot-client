5e0c180aa000717e38bc892810d3ab0d9ec789f5
Lucianos Lionakis <lucianos.lionakis@openanalytics.eu>
2023-04-19 11:51:03 +0200

    change: generalize validatedInput component

--- src/components/common/ValidatedInputField.vue
+++ src/components/common/ValidatedInputField.vue
@@ -1,33 +1,32 @@
 <template>
-  <v-text-field
-    v-if="type === 'text'"
+  <component
+    :is="as"
     v-model="value"
     @blur="handleBlur"
-    :label="label"
-    :error-messages="errors"
-  />
-  <v-select
-    v-if="type === 'select'"
-    v-model="value"
-    @blur="handleBlur"
-    :items="items"
-    :label="label"
     :error-messages="errors"
   />
 </template>
 
 <script setup lang="ts">
 import { useField } from 'vee-validate'
-import { ref, toRef } from 'vue'
+import { toRef } from 'vue'
+import { VSelect, VTextField } from 'vuetify/components'
+import { z } from 'zod'
 
 const props = defineProps<{
   name: string
-  type: string
-  label: string
-  items?: string[]
+  type: Component
 }>()
 
-const items = ref(props.items)
+const component = z.enum(['v-text-field', 'v-select'])
+type Component = z.infer<typeof component>
+
+const toComponent = new Map<Component, any>([
+  [component.enum['v-text-field'], VTextField],
+  [component.enum['v-select'], VSelect]
+])
+
+const as = toComponent.get(props.type)
 const { value, handleBlur, errors } = useField(
   toRef(props, 'name'),
   undefined
