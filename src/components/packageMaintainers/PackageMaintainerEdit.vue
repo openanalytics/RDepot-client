0a91372c929e709b258e812d33ca44e1ceca82b9
Lucianos Lionakis <lucianos.lionakis@openanalytics.eu>
2023-04-19 14:52:27 +0200

    add: validation to package maintainer edit

--- src/components/packageMaintainers/PackageMaintainerEdit.vue
+++ src/components/packageMaintainers/PackageMaintainerEdit.vue
@@ -1,20 +1,28 @@
 <template>
-  <v-card class="pa-5" width="400">
-    <v-card-title>
-      {{ $t('maintainers.editform.title') }}
-    </v-card-title>
-    <v-divider></v-divider>
-    <v-card-text style="height: 300px">
-      <v-form ref="form" lazy-validation>
-        <v-text-field
+  <Form
+    as="v-form"
+    :validation-schema="validationSchema"
+    ref="form"
+    lazy-validation
+  >
+    <v-card class="pa-5" width="400">
+      <v-card-title>
+        {{ $t('maintainers.editform.title') }}
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-card-text style="height: 300px">
+        <validated-input-field
+          type="v-text-field"
+          name="user.name"
           id="edit-package-maintainer-user"
           v-model="localMaintainer.user"
           :value="localMaintainer.user?.name"
           :label="$t('maintainers.editform.user')"
           disabled
-        >
-        </v-text-field>
-        <v-select
+        />
+        <validated-input-field
+          type="v-select"
+          name="repository.id"
           :modelValue="localMaintainer.repository"
           @update:modelValue="newValue => localMaintainer.repository!.id = newValue"
           :items="repositories"
@@ -22,50 +30,60 @@
           item-value="id"
           id="edit-package-maintainer-repository"
           :label="$t('maintainers.editform.repository')"
-        ></v-select>
-        <v-select
+        />
+        <validated-input-field
+          type="v-select"
+          name="packageName"
           id="edit-package-maintainer-package"
           v-model="localMaintainer.packageName"
           :items="packages"
           item-title="name"
           item-value="name"
           :label="$t('maintainers.editform.package')"
-        ></v-select>
-      </v-form>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-card-actions>
-      <v-row justify="space-between" class="mt-1">
-        <v-btn
-          id="cancelbutton"
-          color="blue darken-1"
-          @click="changeDialogOptions"
-          class="mx-1"
-        >
-          <small>
-            {{ $t('common.cancel') }}
-          </small>
-        </v-btn>
-        <v-row class="my-0" justify="end">
+        />
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-row justify="space-between" class="mt-1">
           <v-btn
-            id="setfiltration"
+            id="cancelbutton"
             color="blue darken-1"
+            @click="changeDialogOptions"
             class="mx-1"
-            @click="editMaintainer()"
           >
             <small>
-              {{ $t('common.save') }}
+              {{ $t('common.cancel') }}
             </small>
           </v-btn>
+          <v-row class="my-0" justify="end">
+            <v-btn
+              id="setfiltration"
+              color="blue darken-1"
+              class="mx-1"
+              @click="editMaintainer()"
+            >
+              <small>
+                {{ $t('common.save') }}
+              </small>
+            </v-btn>
+          </v-row>
         </v-row>
-      </v-row>
-    </v-card-actions>
-  </v-card>
+      </v-card-actions>
+    </v-card>
+  </Form>
 </template>
 
 <script setup lang="ts">
 import { usePackageMaintainersStore } from '@/store/package_maintainers'
+import { toTypedSchema } from '@vee-validate/zod'
+import { Form } from 'vee-validate'
+import ValidatedInputField from '../common/ValidatedInputField.vue'
 import { ref, onMounted, computed } from 'vue'
+import { packageMaintainerSchema } from '@/models/Schamas'
+
+const validationSchema = toTypedSchema(
+  packageMaintainerSchema
+)
 
 const maintainers_store = usePackageMaintainersStore()
 
