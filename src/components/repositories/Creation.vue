64bc4e95991216bbfe58680731585b4c29ef83dd
Lucianos Lionakis <lucianos.lionakis@openanalytics.eu>
2023-04-19 10:14:26 +0200

    change: use vee-validate and zod in the repository creation component

--- src/components/repositories/Creation.vue
+++ src/components/repositories/Creation.vue
@@ -1,102 +1,98 @@
 <template>
-  <v-card class="pa-5" width="400">
-    <v-card-title>
-      {{ $t('repositories.creation.title') }}
-    </v-card-title>
-    <v-divider></v-divider>
-    <v-card-text style="height: 300px">
-      <v-form ref="form">
-        <v-text-field
-          class="my-2"
-          id="repository-name"
-          v-model="newRepository.name"
+  <Form
+    as="v-form"
+    :validation-schema="validationSchema"
+    v-slot="{ values }"
+  >
+    <v-card class="pa-5" width="400">
+      <v-card-title>
+        {{ $t('repositories.creation.title') }}
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-card-text>
+        <validated-input-field
+          name="name"
+          type="text"
           :label="$t('repositories.creation.name')"
-        ></v-text-field>
-
-        <v-text-field
-          class="my-5"
-          id="repository-technology"
-          v-model="newRepository.publicationUri"
+        ></validated-input-field>
+        <validated-input-field
+          name="publicationUri"
+          type="text"
           :label="
             $t('repositories.creation.publicationUri')
           "
-          :placeholder="
-            $t(
-              'repositories.creation.publicationUriPlaceholder'
-            )
-          "
-        ></v-text-field>
-        <v-text-field
-          class="my-2"
-          id="filtration-deleted"
-          v-model="newRepository.serverAddress"
+        ></validated-input-field>
+        <validated-input-field
+          name="serverAddress"
+          type="text"
           :label="$t('repositories.creation.serverAddress')"
-          :placeholder="
-            $t(
-              'repositories.creation.serverAddressPlaceholder'
-            )
-          "
-        ></v-text-field>
-        <v-select
-          class="my-2"
-          id="repository-technology"
-          v-model="newRepository.technology"
+        ></validated-input-field>
+        <validated-input-field
           :items="technologySelect"
+          name="technology"
+          type="select"
           :label="$t('repositories.creation.technology')"
-        >
-        </v-select>
-      </v-form>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-card-actions>
-      <v-row justify="space-between" class="mt-1">
-        <v-btn
-          id="cancel-button"
-          color="blue darken-1"
-          @click="changeDialogOptions()"
-          class="mx-1"
-        >
-          <small>
-            {{ $t('common.cancel') }}
-          </small>
-        </v-btn>
-        <v-row class="my-0" justify="end">
+        ></validated-input-field>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-row justify="space-between" class="mt-1">
           <v-btn
-            id="set-filtration"
+            id="cancel-button"
             color="blue darken-1"
+            @click="changeDialogOptions()"
             class="mx-1"
-            @click="createRepository()"
           >
             <small>
-              {{ $t('common.create') }}
+              {{ $t('common.cancel') }}
             </small>
           </v-btn>
+          <v-row class="my-0" justify="end">
+            <v-btn
+              id="set-filtration"
+              color="blue darken-1"
+              class="mx-1"
+              @click="onSubmit(values)"
+            >
+              <small>
+                {{ $t('common.create') }}
+              </small>
+            </v-btn>
+          </v-row>
         </v-row>
-      </v-row>
-    </v-card-actions>
-  </v-card>
+      </v-card-actions>
+    </v-card>
+  </Form>
 </template>
 
 <script setup lang="ts">
 import { useRepositoryStore } from '@/store/repositories'
 import { ref, onMounted } from 'vue'
 import { EntityModelRepositoryDto } from '@/openapi'
-import { TechnologiesEnum } from '@/enum/Technologies'
+import { Technologies } from '@/enum/Technologies'
+import { repositorySchema } from '@/models/Schamas'
+import { toTypedSchema } from '@vee-validate/zod/dist/vee-validate-zod'
+import { Form } from 'vee-validate'
+import ValidatedInputField from '../common/ValidatedInputField.vue'
+
+const validationSchema = toTypedSchema(
+  repositorySchema.pick({
+    name: true,
+    publicationUri: true,
+    serverAddress: true,
+    technology: true
+  })
+)
 
 const repository_store = useRepositoryStore()
 
-const technologySelect = ref(
-  Object.values(TechnologiesEnum)
-)
-
-const newRepository = ref({} as EntityModelRepositoryDto)
+const technologySelect = ref(Technologies.options)
 
 const emit = defineEmits(['closeModal'])
 
-async function createRepository() {
-  await repository_store.createRepository(
-    newRepository.value
-  )
+function onSubmit(values: EntityModelRepositoryDto) {
+  // console.log('Submited with ', values)
+  repository_store.createRepository(values)
   changeDialogOptions()
 }
 
@@ -106,3 +102,9 @@ function changeDialogOptions() {
 
 onMounted(() => {})
 </script>
+
+<style>
+.v-input__details {
+  display: flex;
+}
+</style>
