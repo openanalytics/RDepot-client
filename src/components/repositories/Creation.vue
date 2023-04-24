a3f34979d3ba8572319f9bab1dacdfbfcd3c5d1c
Lucianos Lionakis <lucianos.lionakis@openanalytics.eu>
2023-04-24 15:10:56 +0200

    refactor: use CardActions in repository Creation

--- src/components/repositories/Creation.vue
+++ src/components/repositories/Creation.vue
@@ -48,32 +48,7 @@
       </v-form>
     </v-card-text>
     <v-divider></v-divider>
-    <v-card-actions>
-      <v-row justify="space-between" class="mt-1">
-        <v-btn
-          id="cancel-button"
-          color="blue darken-1"
-          @click="changeDialogOptions()"
-          class="mx-1"
-        >
-          <small>
-            {{ $t('common.cancel') }}
-          </small>
-        </v-btn>
-        <v-row class="my-0" justify="end">
-          <v-btn
-            id="set-filtration"
-            color="blue darken-1"
-            class="mx-1"
-            @click="createRepository()"
-          >
-            <small>
-              {{ $t('common.create') }}
-            </small>
-          </v-btn>
-        </v-row>
-      </v-row>
-    </v-card-actions>
+    <card-actions :buttons="buttons" />
   </v-card>
 </template>
 
@@ -82,12 +57,25 @@ import { useRepositoryStore } from '@/store/repositories'
 import { ref, onMounted } from 'vue'
 import { EntityModelRepositoryDto } from '@/openapi'
 import { TechnologiesEnum } from '@/enum/Technologies'
+import CardActions from '../common/CardActions.vue'
+import { i18n } from '@/plugins/i18n'
 
 const repository_store = useRepositoryStore()
 
 const technologySelect = ref(
   Object.values(TechnologiesEnum)
 )
+
+const buttons = [
+  {
+    text: i18n.t('common.cancel'),
+    handler: changeDialogOptions
+  },
+  {
+    text: i18n.t('common.create'),
+    handler: createRepository
+  }
+]
 
 const newRepository = ref({} as EntityModelRepositoryDto)
 
