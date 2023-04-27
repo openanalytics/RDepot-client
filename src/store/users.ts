afb3389839651268e574ab66632fafc3f8d3d224
Lucianos Lionakis <lucianos.lionakis@openanalytics.eu>
2023-04-27 16:14:57 +0200

    add: fetch users/roles and save user functionality to user store

--- src/store/users.ts
+++ src/store/users.ts
@@ -1,12 +1,23 @@
 import { defineStore } from 'pinia'
 import { LoginApiData } from '@/models/users/Login'
 import { LoginType } from '@/enum/LoginType'
-import { loginApi } from '@/services/users_services'
+import {
+  fetchRoles,
+  fetchUsers,
+  loginApi,
+  updateUser
+} from '@/services/users_services'
+import { EntityModelUserDto, RoleDto } from '@/openapi'
+import { usePaginationStore } from './pagination'
+import { Role } from '@/enum/UserRoles'
 
 interface State {
   userToken: string
   userName: string
   loginType: LoginType
+  userList: EntityModelUserDto[]
+  chosenUser: EntityModelUserDto
+  roles: RoleDto[]
 }
 
 export const useUserStore = defineStore('user_store', {
@@ -14,7 +25,10 @@ export const useUserStore = defineStore('user_store', {
     return {
       userToken: '',
       userName: '',
-      loginType: 'DEFAULT'
+      loginType: 'DEFAULT',
+      userList: [],
+      chosenUser: {},
+      roles: []
     }
   },
   actions: {
@@ -25,6 +39,32 @@ export const useUserStore = defineStore('user_store', {
     },
     chooseLoginType(payload: LoginType) {
       this.loginType = payload
+    },
+    async fetchUsers() {
+      const pagination = usePaginationStore()
+      const [users, pageInfo] = await fetchUsers()
+      this.userList = users
+      pagination.setPage(pageInfo.page)
+      pagination.setTotalNumber(pageInfo.totalNumber)
+    },
+    async fetchRoles() {
+      if (this.roles.length === 0) {
+        const [roles] = await fetchRoles()
+        this.roles = roles
+      }
+    },
+    async saveUser(newUser: EntityModelUserDto) {
+      await updateUser(this.chosenUser, newUser).then(
+        (success: boolean) => {
+          if (success) {
+          }
+        }
+      )
+    },
+    roleIdToRole(roleId: number) {
+      return this.roles.length !== 0
+        ? this.roles[Role.parse(roleId - 1)]
+        : {}
     }
   }
 })
