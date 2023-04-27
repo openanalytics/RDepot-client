c1d7647d49183f590b62fc069e471364489cf451
Lucianos Lionakis <lucianos.lionakis@openanalytics.eu>
2023-04-27 16:14:18 +0200

    ad: fetch users and roles and update user to user service

--- src/services/users_services.ts
+++ src/services/users_services.ts
@@ -1,7 +1,24 @@
-import { Login, LoginApiData } from '@/models/users/Login'
+import { LoginApiData } from '@/models/users/Login'
+import {
+  ApiV2UserControllerApiFactory,
+  EntityModelUserDto,
+  ResponseDtoCollectionModelRoleDto,
+  ResponseDtoPagedModelEntityModelUserDto,
+  RoleDto
+} from '@/openapi'
 import api from '@/plugins/axios'
 import { notify } from '@kyvg/vue3-notification'
-import { createExpect } from 'vitest'
+import { getConfiguration } from './api_config'
+import {
+  openApiRequest,
+  validateRequest,
+  validatedData
+} from './open_api_access'
+import { useSortStore } from '@/store/sort'
+import { isAuthorized } from '@/plugins/casl'
+import { createPatch } from 'rfc6902'
+import { i18n } from '@/plugins/i18n'
+import { AxiosResponse } from 'axios'
 
 export async function loginApi(data: LoginApiData) {
   try {
@@ -32,4 +49,95 @@ export async function loginApi(data: LoginApiData) {
     })
     return error
   }
+}
+
+export async function fetchUsers(
+  page?: number,
+  pageSize?: number
+): Promise<validatedData<EntityModelUserDto>> {
+  if (!isAuthorized('GET', 'users')) {
+    return new Promise(() => validateRequest())
+  }
+  const user_api = ApiV2UserControllerApiFactory(
+    getConfiguration()
+  )
+  const sort = useSortStore()
+
+  return openApiRequest<ResponseDtoPagedModelEntityModelUserDto>(
+    user_api.getAllUsers,
+    [page, pageSize, sort.getSortBy()]
+  ).then(
+    (res) =>
+      validateRequest(
+        res.data.data?.content,
+        res.data.data?.page
+      ),
+    (msg) => {
+      notify({
+        text: msg,
+        type: 'error'
+      })
+      return validateRequest()
+    }
+  )
+}
+
+export async function updateUser(
+  oldUser: EntityModelUserDto,
+  newUser: EntityModelUserDto
+): Promise<boolean> {
+  if (!isAuthorized('PATCH', 'users')) {
+    return new Promise(() => false)
+  }
+  const user_api = ApiV2UserControllerApiFactory(
+    getConfiguration()
+  )
+  const patch = createPatch(oldUser, newUser)
+
+  return openApiRequest<AxiosResponse<any>>(
+    user_api.patchUser,
+    [patch, oldUser.id]
+  ).then(
+    () => {
+      notify({
+        text: i18n.t('notification.successUpdateUser'),
+        type: 'success'
+      })
+      return true
+    },
+    (msg) => {
+      notify({
+        text: msg,
+        type: 'error'
+      })
+      return false
+    }
+  )
+}
+
+export async function fetchRoles(): Promise<
+  validatedData<RoleDto>
+> {
+  if (!isAuthorized('GET', 'users')) {
+    return new Promise(() => validateRequest())
+  }
+  const user_api = ApiV2UserControllerApiFactory(
+    getConfiguration()
+  )
+  console.log('user_api: ')
+  return openApiRequest<ResponseDtoCollectionModelRoleDto>(
+    user_api.getRoles
+  ).then(
+    (res) => {
+      console.log('res: ', res)
+      return validateRequest(res.data.data?.content)
+    },
+    (msg) => {
+      notify({
+        text: msg,
+        type: 'error'
+      })
+      return validateRequest()
+    }
+  )
 }
