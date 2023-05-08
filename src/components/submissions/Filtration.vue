17db015ef24702335c54c9606d40f24389f1ff60
kkulesa <katarzyna.kulesa@openanalytics.eu>
2023-05-08 20:23:36 +0200

    change: use custom select component and validated-input-fields

--- src/components/submissions/Filtration.vue
+++ src/components/submissions/Filtration.vue
@@ -5,61 +5,84 @@
     v-on:set-filtration="setFiltration()"
     v-on:change-dialog-options="changeDialogOptions()"
   >
-    <v-select
-      id="filtration-state"
-      v-model="localFiltration.state"
-      :items="stateSelect"
-      :label="$t('submissions.filtration.state')"
-    ></v-select>
-    {{ localFiltration.package }}
-    <v-combobox
-      id="filtration-repository"
-      v-model="localFiltration.package"
-      :items="package_store.packages"
-      item-title="name"
-      filter-keys="name"
-      :placeholder="placeholder"
-      :hide-no-data="false"
-      persistent-hint
-      :menu-props="{
-        location: 'bottom center',
-        height: '200px'
-      }"
-      :label="$t('submissions.filtration.package')"
-      autofocus
-      @update:search="updateSearch($event)"
-      return-object
-    >
-    </v-combobox>
-
-    <v-switch
-      v-model="localFiltration.assignedToMe"
-      color="oablue"
-      :label="$t('submissions.filtration.assigned')"
-    >
-    </v-switch>
+    <form as="v-form" lazy-validation>
+      <validated-input-field
+        id="filtration-state"
+        :items="stateSelect"
+        name="state"
+        as="v-select"
+        :label="$t('submissions.filtration.state')"
+      ></validated-input-field>
+      <SelectField
+        name="package"
+        :label="$t('submissions.filtration.package')"
+        v-on:loadItems="loadPackages"
+        v-on:filtrate="filtratePackages"
+        store_id="packages"
+      />
+      <validated-input-field
+        name="assignedToMe"
+        :label="$t('submissions.filtration.assigned')"
+        :items="stateSelect"
+        as="v-switch"
+      ></validated-input-field>
+    </form>
   </filtration-card>
 </template>
 
 <script setup lang="ts">
-import { EntityModelSubmissionDtoStateEnum } from '@/openapi'
-import { usePackagesStore } from '@/store/packages'
+import {
+  EntityModelPackageDto,
+  EntityModelSubmissionDtoStateEnum
+} from '@/openapi'
 import { useSubmissionStore } from '@/store/submission'
 import { ref, onMounted } from 'vue'
 import { useObjectActions } from '@/composable/objectActions'
 import FiltrationCard from '@/components/common/FiltrationCard.vue'
+import SelectField from '@/components/common/fields/SelectField.vue'
+import { useForm } from 'vee-validate'
+import ValidatedInputField from '@/components/common/ValidatedInputField.vue'
+import { SubmissionsFiltration } from '@/models/Filtration'
+import { usePackagesStore } from '@/store/packages'
+import { useSelectStore } from '@/store/select_pagination'
 
 const { setAllFields } = useObjectActions()
 const submissions_store = useSubmissionStore()
-const package_store = usePackagesStore()
+const packages_store = usePackagesStore()
+const select_store = useSelectStore('packages')
 
 const stateSelect = ref(
   Object.values(EntityModelSubmissionDtoStateEnum)
 )
+
 const localFiltration = ref(submissions_store.filtration)
-const search = ref('')
-const placeholder = ref('')
 const emit = defineEmits(['closeModal'])
+
+const { meta, values } = useForm({
+  validationSchema: {
+    state: EntityModelSubmissionDtoStateEnum,
+    package: String,
+    assignedToMe: Boolean
+  }
+})
+
+async function loadPackages() {
+  select_store.paginationData =
+    await packages_store.fetchPageOfPackages(
+      select_store.paginationData.page
+    )
+  select_store.addItems(
+    packages_store.packages.map(
+      (packageBag: EntityModelPackageDto) => packageBag.name
+    )
+  )
+}
+
+function filtratePackages(value: string | undefined) {
+  if (packages_store.filtration.repository !== value) {
+    packages_store.setFiltrationByRepositoryOnly(value)
+  }
+}
 
 function updateFiltration() {
   localFiltration.value = JSON.parse(
@@ -67,16 +90,15 @@ function updateFiltration() {
   )
 }
 
-function updateSearch(value: string) {
-  // if (value.length > 2) {
-  // alert(value)
-  // package_store.fetchAllPackages()
-  // }
-}
-
 function setFiltration() {
+  var submissionFiltration: SubmissionsFiltration = {
+    state: values.state,
+    package: values.package,
+    assignedToMe: values.assignedToMe
+  }
+  console.log(values)
   emit('closeModal')
-  submissions_store.setFiltration(localFiltration.value)
+  submissions_store.setFiltration(submissionFiltration)
 }
 
 function changeDialogOptions() {
@@ -86,8 +108,6 @@ function changeDialogOptions() {
 
 onMounted(async () => {
   updateFiltration()
-  package_store.fetchPackages()
-  await package_store.fetchPackages()
 })
 
 function clearFiltration() {
