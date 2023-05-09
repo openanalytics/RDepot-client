442ed3e7448f65486f2b490973f9e58fcd956390
kkulesa <katarzyna.kulesa@openanalytics.eu>
2023-05-09 19:01:15 +0200

    change: update validate-input-field with new field and attrs option

--- src/components/common/ValidatedInputField.vue
+++ src/components/common/ValidatedInputField.vue
@@ -4,6 +4,7 @@
     v-model="value"
     @blur="handleBlur"
     :error-messages="errors"
+    v-bind="attrs"
   />
 </template>
 
@@ -16,23 +17,27 @@ import {
   VTextField
 } from 'vuetify/components'
 import { z } from 'zod'
+import AutocompleteField from '@/components/common/fields/AutocompleteField.vue'
 
 const props = defineProps<{
   name: string
   as: Component
+  attrs?: Object
 }>()
 
 const component = z.enum([
   'v-text-field',
   'v-select',
-  'v-switch'
+  'v-switch',
+  'autocomplete'
 ])
 type Component = z.infer<typeof component>
 
 const toComponent = new Map<Component, any>([
   [component.enum['v-text-field'], VTextField],
   [component.enum['v-select'], VSelect],
-  [component.enum['v-switch'], VSwitch]
+  [component.enum['v-switch'], VSwitch],
+  [component.enum['autocomplete'], AutocompleteField]
 ])
 
 const as = toComponent.get(props.as)
