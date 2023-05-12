b716d2492676c4bf34121b3146aa311b48a542f9
kkulesa <katarzyna.kulesa@openanalytics.eu>
2023-05-12 13:18:05 +0200

    feat: chips with filtration state (after click the filtration rule is cancelled)

---
+++ src/components/common/chips/Chip.vue
@@ -0,0 +1,42 @@
+<template>
+  <span class="chip-section">
+    <v-chip class="pa-4 mr-3" @click="$emit('update')" link>
+      {{ label }} <span v-if="value">: {{ value }}</span>
+    </v-chip>
+    <v-icon
+      icon="mdi-close-circle"
+      size="s"
+      @click="$emit('update')"
+      class="icon"
+    ></v-icon>
+  </span>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  label: String,
+  value: {
+    type: Object as () => String | Boolean | String[],
+    required: false
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.chip-section {
+  position: relative;
+  .icon {
+    display: none;
+    z-index: 10;
+    position: absolute;
+    right: 8px;
+    top: -8px;
+  }
+
+  &:hover {
+    .icon {
+      display: block;
+    }
+  }
+}
+</style>
---
+++ src/components/common/chips/FiltrationChips.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-row class="ml-10 mt-5">
+    <div
+      v-for="(field, i) in values"
+      :key="i"
+      v-show="field != undefined"
+    >
+      <chip
+        v-if="typeof field == 'boolean' && field != false"
+        :label="labels?.get(i)"
+        v-on:update="updateFiltration(i, !field)"
+      />
+      <chip
+        v-else-if="typeof field == 'string'"
+        :label="labels?.get(i)"
+        v-on:update="updateFiltration(i)"
+        :value="field"
+      />
+      <span
+        v-else-if="typeof field == 'object'"
+        v-for="(value, index) in field"
+        :key="index"
+      >
+        <chip
+          :label="labels?.get(i)"
+          :value="value.toString()"
+          v-on:update="updateFiltration(i, value, field)"
+        />
+      </span>
+    </div>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+import Chip from './Chip.vue'
+
+const props = defineProps({
+  values: Object,
+  labels: Map<string, string>
+})
+
+const emit = defineEmits(['update'])
+
+function updateFiltration(
+  fieldName: string,
+  value?: string | boolean,
+  field?: string[]
+) {
+  if (field && value) {
+    field = field.filter((fieldValue) => {
+      return fieldValue != value
+    })
+    emit('update', fieldName, field)
+  } else if (value != undefined) {
+    emit('update', fieldName, value)
+  } else {
+    emit('update', fieldName)
+  }
+}
+</script>
