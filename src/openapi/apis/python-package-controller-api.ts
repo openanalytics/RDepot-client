29102c749d23ed895bd40fa842379d5e323c5be7
kkulesa <katarzyna.kulesa@openanalytics.eu>
2024-03-29 11:38:42 +0100

    fix: license header + ts syntaxt

--- src/openapi/apis/python-package-controller-api.ts
+++ src/openapi/apis/python-package-controller-api.ts
@@ -1,3 +1,24 @@
+/*
+ * R Depot
+ *
+ * Copyright (C) 2012-2024 Open Analytics NV
+ *
+ * ===========================================================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License as published by
+ * The Apache Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Apache License for more details.
+ *
+ * You should have received a copy of the Apache License
+ * along with this program. If not, see <http://www.apache.org/licenses/>
+ *
+ */
 /* tslint:disable */
 /* eslint-disable */
 /**
@@ -499,8 +520,18 @@ export const PythonPackageControllerApiAxiosParamCreator =
         }
         const needsSerialization =
           typeof body !== 'string' ||
-          localVarRequestOptions.headers['Content-Type'] ===
-            'application/json'
+          Object.entries(
+            localVarRequestOptions.headers!
+          ).find(([key, value]) => {
+            if (
+              value === 'application/json' &&
+              key == 'Content-Type'
+            ) {
+              return true
+            }
+            return false
+          })
+
         localVarRequestOptions.data = needsSerialization
           ? JSON.stringify(body !== undefined ? body : {})
           : body || ''
