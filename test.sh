6c04d21c9e36e7b9bb920e35d322b0471debb32b
wmrozek <weronika.mrozek@openanalytics.eu>
2024-05-30 16:55:44 +0200

    fix: add commands to test script

--- test.sh
+++ test.sh
@@ -4,5 +4,8 @@ while [ "$(docker inspect -f {{.State.Health.Status}} oa-rdepot-backend)" != "he
     sleep 1.0;
 done;
 apt-get install chromium-driver
+apt-get install default-jre
+apt-get -f install
+apt-get install chromium-browser
 apt update -y
 echo "Backend is ready"
