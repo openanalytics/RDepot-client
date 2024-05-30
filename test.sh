d3f466797ebc09d1f8a3b2449bda4267f9c14285
wmrozek <weronika.mrozek@openanalytics.eu>
2024-05-30 16:41:53 +0200

    fix: add sudo apt to test script

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ docker-compose -f docker-compose-test.yml up -d
 while [ "$(docker inspect -f {{.State.Health.Status}} oa-rdepot-backend)" != "healthy" ]; do
     sleep 1.0;
 done;
-
+sudo apt-get install chromium-driver
 echo "Backend is ready"
