424967e0dbb0a4465fa7e1bbf1ac15b336e76203
kkulesa <katarzyna.kulesa@openanalytics.eu>
2024-10-07 14:57:28 +0200

    design: remove the top padding from the code tag (looked asymmetrically)

--- src/components/packages/packageDetails/PackageInstallation.vue
+++ src/components/packages/packageDetails/PackageInstallation.vue
@@ -32,7 +32,7 @@
     <div class="code mb-2 mt-4 mr-2 ml-1">
       <code
         id="install-command"
-        class="d-flex justify-lg-space-between pt-7"
+        class="d-flex justify-lg-space-between"
       >
         {{ installCode }}
         <v-tooltip location="left">
